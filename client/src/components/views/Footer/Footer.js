612f7aa7817bd9ada3eb6cbdbf8f4fa6a06ad62d
mithun1999 <mithunkumari@karunya.edu.in>
2020-10-07 22:19:09 +0530

    initial commit

---
+++ client/src/components/views/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Icon} from 'antd';
+
+function Footer() {
+    return (
+        <div style={{
+            height: '80px', display: 'flex',
+            flexDirection: 'column', alignItems: 'center',
+            justifyContent: 'center', fontSize:'1rem'
+        }}>
+           <p> Made with  <Icon type="smile" />  by Mithun</p>
+        </div>
+    )
+}
+
+export default Footer
